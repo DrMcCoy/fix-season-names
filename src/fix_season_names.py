4f152183744ffb231fe51adb53a719f7541ef41c
Sven Hesse <drmccoy@drmccoy.de>
2024-05-25 15:14:17 +0200

    Add empty project frame

---
+++ src/fix_season_names.py
@@ -0,0 +1,35 @@
+"""! The actual Fix Season Names logic.
+"""
+
+# Fix Season Names - Fix TV season names in Jellyfin
+#
+# Fix Season Names is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+
+
+class FixSeasonNames:  # pylint: disable=too-few-public-methods
+    """! Fix Season Names logic.
+    """
+
+    def __init__(self, _: argparse.Namespace) -> None:
+        pass
+
+    def run(self) -> None:
+        """! Run the main logic.
+        """
